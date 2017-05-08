0cdda4e9f8619cac5d0b295f4ecdbcf0b56b4cd7
helenwalsh <helen.walsh@emc.com>
2017-05-08 16:35:29 +0100

    Live Migration rollback fix

--- emc_vmax_masking.py
+++ emc_vmax_masking.py
@@ -2819,10 +2819,10 @@ class EMCVMAXMasking(object):
             self._get_port_group_from_masking_view(
                 conn, maskingViewName, storageSystemName))
         if portGroupInstanceName is None:
-            LOG.error(_LE(
-                "Cannot get port group from masking view: "
-                "%(maskingViewName)s. "),
-                {'maskingViewName': maskingViewName})
+            errorMessage = (_("Cannot get port group from masking view: "
+                            "%(maskingViewName)s.") %
+                            {'maskingViewName': maskingViewName})
+            LOG.error(errorMessage)
         else:
             try:
                 portGroupInstance = (
@@ -2830,8 +2830,8 @@ class EMCVMAXMasking(object):
                 portGroupName = (
                     portGroupInstance['ElementName'])
             except Exception:
-                LOG.error(_LE(
-                    "Cannot get port group name."))
+                errorMessage = (_("Cannot get port group name."))
+                LOG.error(errorMessage)
         return portGroupName, errorMessage
 
     @coordination.synchronized('emc-sg-'
